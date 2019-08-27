fc4785e93c343594e7c9946d33ecd3f603b0a998
TristanEugene <flp.vndrstrtn@gmail.com>
2019-08-27 11:00:28 +0200

    10

--- 05-arrays/11-dedupe-array/script.js
+++ 05-arrays/11-dedupe-array/script.js
@@ -9,7 +9,7 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     var fruits = [
         "cerise",
@@ -29,5 +29,31 @@
     ];
 
     // your code here
+    var button = document.getElementById("run");
+    button.addEventListener("click", function () {
+
+        function find_duplicate_in_array(arra1) {
+            var object = {};
+            var result = [];
+
+            arra1.forEach(function (item) {
+                if (!object[item])
+                    object[item] = 0;
+                object[item] += 1;
+            })
+
+            for (var prop in object) {
+                if (object[prop] >= 2) {
+                    result.push(prop);
+                }
+            }
+
+            return result;
+
+        }
+
+        console.log(find_duplicate_in_array(["cerise", "durian", "pomme", "poire", "fraise", "tomate", "orange", "mandarine", "fraise", "durian", "pêche", "cerise", "raisin", "cerise",]));
+
+    })
 
 })();
