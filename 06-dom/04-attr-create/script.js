c092c02dc03948639c95767d3d1fb9ac060da2ed
TristanEugene <flp.vndrstrtn@gmail.com>
2019-08-28 16:46:21 +0200

    6.4

--- 06-dom/04-attr-create/script.js
+++ 06-dom/04-attr-create/script.js
@@ -12,5 +12,19 @@
 (function() {
 
     // your code here
+    // Get the value of the "data-image" attribute from the element with id "source"
+    let getId = document.getElementById("source");
+    let getAttr = getId.getAttribute("data-image");
+
+    // console.log(getAttr)
+
+    // Create a new image element inside the target element
+    // that displays the image referred to by the attribute value
+    // Then remove the original element
+    let newImg = document.createElement("img");
+    newImg.setAttribute("src",getAttr );
+    console.log(newImg);
+    document.getElementById("target").appendChild(newImg);
+    getId.remove();
 
 })();
