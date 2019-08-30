167b58d3b754ba9668421501a1a7d10f3d9c40e1
TristanEugene <flp.vndrstrtn@gmail.com>
2019-08-30 11:14:54 +0200

    6.12

--- 06-dom/12-change-event-input-two/script.js
+++ 06-dom/12-change-event-input-two/script.js
@@ -8,9 +8,32 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-
 (function() {
 
     // your code here
+    var validity = document.getElementById("validity");
+
+    document.getElementById("pass-one").addEventListener("input", function () {
+
+        var input = document.getElementById("pass-one").value;
+        var counter = 0;
+
+        for(var i =0; i < input.length; i++){
+            var check = input.charAt(i);
+
+            if (!isNaN(check)){
+                counter++;
+            }
+        }
+
+
+        if (input.length >= 8 && counter >= 2){
+            validity.innerHTML = "ok"
+        }
+        else {
+            validity.innerHTML = "not ok"
+        }
+
+    })
 
 })();
