3a3066110e9bd0010a57eb707c36b996e0924525
TristanEugene <flp.vndrstrtn@gmail.com>
2019-09-03 11:12:08 +0200

    8.1

--- 08-asynchronous/01-get-posts/script.js
+++ 08-asynchronous/01-get-posts/script.js
@@ -11,4 +11,20 @@
 
 (() => {
     // your code here
+
+    /*
+    function myCallback(error, articles) {
+        console.log(articles);
+    }
+     */
+
+    document.getElementById("run").addEventListener("click", function (){
+        window.lib.getPosts(function (error, articles) {
+            // myCallback(error, articles);
+
+            console.log(articles);
+
+        });
+
+    });
 })();
